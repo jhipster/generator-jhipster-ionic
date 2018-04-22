221f5eb966a4b542b8d12c68f9f13c0d78afe840
P. Mohan <pmverma.cs@gmail.com>
2018-04-23 01:01:57 +0630

    added close span tag, correct import (#37)
    * added close span tag >
    
    * correct import error

--- generators/entity-client/templates/ionic/src/pages/entities/_entity.module.ts
+++ generators/entity-client/templates/ionic/src/pages/entities/_entity.module.ts
@@ -19,7 +19,7 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
-import { <%= entityAngularName %>Page } from './<%=entityInstance %>';
+import { <%= entityAngularName %>Page } from './<%=entityFileName %>';
 import { <%= entityAngularName %>Service } from './<%= entityFileName %>.provider';
 
 @NgModule({
