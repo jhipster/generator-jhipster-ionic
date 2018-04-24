2734de1b6ed2d958619f62220af730ba109f06eb
Matt Raible <matt.raible@okta.com>
2018-04-24 00:25:59 -0600

    Add Travis tests with JHipster backend

---
+++ travis/install/01-install-jhipster-stack.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -ev
+
+#-------------------------------------------------------------------------------
+# Choose the repo
+#-------------------------------------------------------------------------------
+export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
+export JHIPSTER_BRANCH=master
+export IONIC4J_REPO=https://github.com/oktadeveloper/generator-jhipster-ionic.git
+export IONIC4J_BRANCH=master
+
+#-------------------------------------------------------------------------------
+# Clone JHipster
+#-------------------------------------------------------------------------------
+cd "$HOME"
+git clone $JHIPSTER_REPO generator-jhipster
+cd generator-jhipster
+if [ "$JHIPSTER_BRANCH" == "latest" ]; then
+    LATEST=$(git describe --abbrev=0)
+    git checkout -b $LATEST $LATEST
+elif [ "$JHIPSTER_BRANCH" != "master" ]; then
+    git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
+fi
+git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+npm install
+npm link
+
+#-------------------------------------------------------------------------------
+# Install JHipster
+#-------------------------------------------------------------------------------
+cd "$TRAVIS_BUILD_DIR"
+npm link generator-jhipster
+npm install
+npm link
+npm run lint
+
+#-------------------------------------------------------------------------------
+# Clone Ionic for JHipster
+#-------------------------------------------------------------------------------
+cd "$HOME"
+git clone $IONIC4J_REPO generator-jhipster-ionic
+cd generator-jhipster-ionic
+if [ "$IONIC4J_BRANCH" == "latest" ]; then
+    LATEST=$(git describe --abbrev=0)
+    git checkout -b $LATEST $LATEST
+elif [ "$IONIC4J_BRANCH" != "master" ]; then
+    git checkout -b $IONIC4J_BRANCH origin/$IONIC4J_BRANCH
+fi
+git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+npm install
+npm link
