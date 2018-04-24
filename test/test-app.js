9a7552adf904697fd3b2126683c07fcb6db80095
Matt Raible <matt.raible@okta.com>
2018-04-24 00:47:13 -0600

    Test progress

--- test/test-app.js
+++ test/test-app.js
@@ -5,17 +5,22 @@ const fse = require('fs-extra');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
-xdescribe('JHipster generator jhipster-generator-ionic', () => {
+describe('JHipster generator jhipster-generator-ionic', () => {
     describe('Test Generating Ionic App', () => {
         beforeEach((done) => {
             helpers
                 .run(path.join(__dirname, '../generators/app'))
                 .inTmpDir((dir) => {
-                    fse.copySync(path.join(__dirname, '../test/templates/backend'), dir);
+                    fse.copySync(path.join(__dirname, '../test/templates'), dir);
+                })
+                .withOptions({
+                    skipInstall: true,
+                    force: true,
+                    testmode: true
                 })
                 .withPrompts({
                     ionicAppName: 'ionic4j',
-                    directoryPath: './templates/backend'
+                    directoryPath: 'backend'
                 })
                 .on('end', done);
         });
