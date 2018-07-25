11d74a19fe9ead52b5a95a6554a7f548b00bec3e
Matt Raible <matt.raible@okta.com>
2018-07-25 09:52:35 +0300

    Remove check for beta release

--- generators/app/index.js
+++ generators/app/index.js
@@ -120,7 +120,7 @@ module.exports = class extends BaseGenerator {
 
         const currentJhipsterVersion = this.jhipsterAppConfig.jhipsterVersion;
         const minimumJhipsterVersion = packagejs.dependencies['generator-jhipster'];
-        if (!currentJhipsterVersion.includes('5.0.0-beta') && !semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
+        if (!semver.satisfies(currentJhipsterVersion, minimumJhipsterVersion)) {
             this.error(`\nYour backend uses an old JHipster version (${currentJhipsterVersion})... you need at least (${minimumJhipsterVersion})\n`);
         }
 
