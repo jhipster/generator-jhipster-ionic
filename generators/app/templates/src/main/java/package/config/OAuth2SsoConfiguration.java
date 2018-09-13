70697c0801493fa8e3722b1e2837ac0822b532bc
Matt Raible <matt.raible@okta.com>
2018-09-13 15:24:39 +0100

    Fix header

--- generators/app/templates/src/main/java/package/config/OAuth2SsoConfiguration.java
+++ generators/app/templates/src/main/java/package/config/OAuth2SsoConfiguration.java
@@ -1,4 +1,8 @@
-package com.okta.developer.gateway.config;
+<%#
+    Copyright 2013-2018 the original author or authors from the JHipster project.
+
+    This file is part of the JHipster project, see http://www.jhipster.tech/
+    for more information.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -11,7 +15,7 @@ package com.okta.developer.gateway.config;
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-    -%>
+-%>
 package <%=packageName%>.config;
 
 import <%=packageName%>.security.AuthoritiesConstants;
