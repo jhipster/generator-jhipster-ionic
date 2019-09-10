1e1ee51d7912e38a007d6290ccc020d8f65843d3
Matt Raible <matt.raible@okta.com>
2019-09-10 01:55:37 +0100

    Try to suppress error: Cannot read property 'blueprint' of undefined

--- generators/app/index.js
+++ generators/app/index.js
@@ -61,13 +61,9 @@ module.exports = class extends BaseGenerator {
         }
       },
       readConfig() {
-        this.jhipsterAppConfig = this.getAllJhipsterConfig();
+        this.jhipsterAppConfig = this.getAllJhipsterConfig(this);
       },
       displayLogo() {
-        // it's here to show that you can use functions from generator-jhipster
-        // this function is in: generator-jhipster/generators/generator-base.js
-        // this.printJHipsterLogo();
-
         // Have Yeoman greet the user.
         this.log(`\nWelcome to the ${chalk.bold.blue('Ionic')} Module for ${chalk.bold.green('J')}${chalk.bold.red('Hipster')}! ${chalk.yellow(`v${packagejs.version}\n`)}`);
       }
