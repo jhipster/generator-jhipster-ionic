60ba0f4cff05fd284c4c58545e60b609f1003bbf
Matt Raible <matt.raible@okta.com>
2019-10-29 12:35:38 -0600

    Use latest JHipster release instead of master branch

--- travis/install/01-install-jhipster-stack.sh
+++ travis/install/01-install-jhipster-stack.sh
@@ -4,27 +4,32 @@ set -ev
 #-------------------------------------------------------------------------------
 # Choose the repo
 #-------------------------------------------------------------------------------
-export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
-export JHIPSTER_BRANCH=master
-export IONIC4J_REPO=https://github.com/${TRAVIS_PULL_REQUEST_SLUG:-$TRAVIS_REPO_SLUG}.git
-export IONIC4J_BRANCH=$BRANCH
+#export JHIPSTER_REPO=https://github.com/jhipster/generator-jhipster.git
+#export JHIPSTER_BRANCH=master
+#export IONIC4J_REPO=https://github.com/${TRAVIS_PULL_REQUEST_SLUG:-$TRAVIS_REPO_SLUG}.git
+#export IONIC4J_BRANCH=$BRANCH
 echo $IONIC4J_REPO
 echo $IONIC4J_BRANCH
 
 #-------------------------------------------------------------------------------
 # Clone JHipster
 #-------------------------------------------------------------------------------
-cd "$HOME"
-git clone $JHIPSTER_REPO generator-jhipster
-cd generator-jhipster
-if [ "$JHIPSTER_BRANCH" == "latest" ]; then
-    LATEST=$(git describe --abbrev=0)
-    git checkout -b $LATEST $LATEST
-elif [ "$JHIPSTER_BRANCH" != "master" ]; then
-    git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
-fi
-git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
-npm link
+#cd "$HOME"
+#git clone $JHIPSTER_REPO generator-jhipster
+#cd generator-jhipster
+#if [ "$JHIPSTER_BRANCH" == "latest" ]; then
+#    LATEST=$(git describe --abbrev=0)
+#    git checkout -b $LATEST $LATEST
+#elif [ "$JHIPSTER_BRANCH" != "master" ]; then
+#    git checkout -b $JHIPSTER_BRANCH origin/$JHIPSTER_BRANCH
+#fi
+#git --no-pager log -n 10 --graph --pretty='%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+#npm link
+
+#-------------------------------------------------------------------------------
+# Install most recent version of JHipster
+#-------------------------------------------------------------------------------
+npm install -g generator-jhipster
 
 #-------------------------------------------------------------------------------
 # Clone Ionic for JHipster
