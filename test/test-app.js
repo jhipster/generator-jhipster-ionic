9734d162c46ca6a14352c9829e03a0877e68a977
Matt Raible <matt.raible@okta.com>
2019-11-23 15:22:10 -0700

    Reformat to use 2 spaces

--- test/test-app.js
+++ test/test-app.js
@@ -6,30 +6,30 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('JHipster generator jhipster-generator-ionic', () => {
-    describe('Test Generating Ionic App with JWT', () => {
-        beforeEach((done) => {
-            helpers
-                .run(path.join(__dirname, '../generators/app'))
-                .inTmpDir((dir) => {
-                    fse.copySync(path.join(__dirname, '../test/templates'), dir);
-                })
-                .withOptions({
-                    interactive: false,
-                    installDeps: false
-                })
-                .withPrompts({
-                    appName: 'ionic4j-jwt',
-                    directoryPath: 'backend-jwt'
-                })
-                .on('end', done);
-        });
-
-        it('generates a ionic4j-jwt/package.json file', () => {
-            assert.file(['ionic4j-jwt/package.json']);
-        });
-
-        it('does not delete app/services/auth/auth-jwt.service.ts', () => {
-            assert.file('ionic4j-jwt/src/app/services/auth/auth-jwt.service.ts');
-        });
+  describe('Test Generating Ionic App with JWT', () => {
+    beforeEach((done) => {
+      helpers
+        .run(path.join(__dirname, '../generators/app'))
+        .inTmpDir((dir) => {
+          fse.copySync(path.join(__dirname, '../test/templates'), dir);
+        })
+        .withOptions({
+          interactive: false,
+          installDeps: false
+        })
+        .withPrompts({
+          appName: 'ionic4j-jwt',
+          directoryPath: 'backend-jwt'
+        })
+        .on('end', done);
     });
+
+    it('generates a ionic4j-jwt/package.json file', () => {
+      assert.file(['ionic4j-jwt/package.json']);
+    });
+
+    it('does not delete app/services/auth/auth-jwt.service.ts', () => {
+      assert.file('ionic4j-jwt/src/app/services/auth/auth-jwt.service.ts');
+    });
+  });
 });
