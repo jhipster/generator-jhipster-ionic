64b2e813c0917cc487c966668f597b9b048fdec3
Matt Raible <matt.raible@okta.com>
2020-03-24 13:34:28 -0600

    Fix mismatched form field names

--- generators/import-jdl/index.js
+++ generators/import-jdl/index.js
@@ -30,7 +30,7 @@ const fs = require('fs-extra');
 function importJDL() {
   logger.info('The JDL is being parsed...');
 
-  const jdlImporter = jhiCore.jdl.import.JDLImporter.createImporterFromFiles(this.jdlFiles, {
+  const jdlImporter = jhiCore.JDLImporter.createImporterFromFiles(this.jdlFiles, {
     databaseType: this.prodDatabaseType,
     applicationType: this.applicationType,
     applicationName: this.baseName
@@ -132,8 +132,8 @@ class ImportJDLGenerator extends BaseGenerator {
         }
         try {
           logger.info(
-            `Generating ${this.importState.exportedEntities.length} ` +
-            `${pluralize('entity', this.importState.exportedEntities.length)}.`
+            `Generating ${this.importState.exportedEntities.length} `
+            + `${pluralize('entity', this.importState.exportedEntities.length)}.`
           );
           this.importState.exportedEntities.forEach((exportedEntity) => {
             generateEntityFiles(this, exportedEntity);
@@ -141,6 +141,14 @@ class ImportJDLGenerator extends BaseGenerator {
         } catch (error) {
           logger.error(`Error while generating entities from the parsed JDL\n${error}`, error);
         }
+      }
+    };
+  }
+
+  get end() {
+    return {
+      runPrettier() {
+        shelljs.exec('npm run prettier');
       }
     };
   }
