48dec966b5a9fa7a443415d2a7d9e50912735812
Matt Raible <matt.raible@okta.com>
2020-03-24 00:14:18 -0600

    Add debug output

--- test-integration/scripts/01-generate-project.sh
+++ test-integration/scripts/01-generate-project.sh
@@ -23,5 +23,7 @@ jhipster import-jdl "$JHIPSTER".jdl --force --no-insight --skip-checks --skip-gi
 cd "$HOME"
 yo jhipster-ionic default --force --no-insight
 cd "$IONIC_FOLDER"
+echo "Running JDL import with $APP_FOLDER/$JHIPSTER.jdl"
+ls "$APP_FOLDER"/"$JHIPSTER".jdl
 yo jhipster-ionic:import-jdl "$APP_FOLDER"/"$JHIPSTER".jdl
 
