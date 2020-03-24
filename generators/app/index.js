e7330623cb8a8443e1fb20eb25e3e27367f35cbd
Matt Raible <matt.raible@okta.com>
2020-03-24 00:43:29 -0600

    Giddyup

--- generators/app/index.js
+++ generators/app/index.js
@@ -135,12 +135,12 @@ module.exports = class extends BaseGenerator {
     const params = ['start', this.ionicAppName, 'oktadeveloper/jhipster'];
     params.push('--type');
     params.push('angular');
+    params.push('--no-deps');
     if (!this.interactive) {
       params.push('--no-interactive');
       params.push('--quiet');
     }
     if (!this.installDeps) {
-      params.push('--no-deps');
       params.push('--no-git');
     }
     spawn.sync('ionic', params, {stdio: 'inherit'});
@@ -284,7 +284,7 @@ module.exports = class extends BaseGenerator {
     return {
       runPrettier() {
         if (this.installDeps) {
-          shelljs.exec(`cd ${this.ionicAppName} && npm run prettier`);
+          shelljs.exec(`cd ${this.ionicAppName} && npm i && npm run prettier`);
         }
       },
 
