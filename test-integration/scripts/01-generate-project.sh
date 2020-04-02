154e6640e77eff7bfbec3cbaddc148c2c259062a
Angelo Manganiello <angelo.mang@libero.it>
2020-04-02 19:52:19 +0200

    Feature/entity (#287)
    * added unit tests and e2e for entities
    
    * change version to run in travis
    
    * modify entity run sh to travis oauth2 e2e
    
    * fix to run e2e tests without refresh page command
    
    * updated templates year
    
    * update unit entity year tests
    
    * fix protractor issue latency to scroll page
    
    * removed all blanck lines
    
    * run prettier
    
    * replace click button in javascript

--- test-integration/scripts/01-generate-project.sh
+++ test-integration/scripts/01-generate-project.sh
@@ -23,5 +23,7 @@ jhipster import-jdl "$JHIPSTER".jdl --force --no-insight --skip-checks --skip-gi
 cd "$HOME"
 yo jhipster-ionic default --force --no-insight
 cd "$IONIC_FOLDER"
+# to use last version linked
+npm link generator-jhipster-ionic
 yo jhipster-ionic:import-jdl "$APP_FOLDER"/"$JHIPSTER".jdl
 
