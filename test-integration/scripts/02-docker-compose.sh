d36f8a9398479c042c0c5a99dfcdb835cb8d63fa
Matt Raible <matt.raible@okta.com>
2021-03-16 12:46:41 -0600

    Update docker-compose.sh with order/images from main generator (#459)

--- test-integration/scripts/02-docker-compose.sh
+++ test-integration/scripts/02-docker-compose.sh
@@ -5,17 +5,28 @@ set -e
 # Start docker container
 #-------------------------------------------------------------------------------
 cd "$HOME/$APP_FOLDER"
-if [ -a src/main/docker/jhipster-registry.yml ]; then
-    docker-compose -f src/main/docker/jhipster-registry.yml up -d
+if [ -a src/main/docker/keycloak.yml ]; then
+    docker-compose -f src/main/docker/keycloak.yml up -d
+fi
+if [ -a src/main/docker/elasticsearch.yml ]; then
+    docker-compose -f src/main/docker/elasticsearch.yml up -d
+fi
+if [ -a src/main/docker/kafka.yml ]; then
+    docker-compose -f src/main/docker/kafka.yml up -d
 fi
 if [ -a src/main/docker/consul.yml ]; then
     docker-compose -f src/main/docker/consul.yml up -d
 fi
 if [ -a src/main/docker/cassandra.yml ]; then
-    docker-compose -f src/main/docker/cassandra.yml up -d
+    # this container can't be started otherwise, as it will take a lot of memory
+    # so here, only prepare the image
+    docker-compose -f src/main/docker/cassandra.yml build
 fi
 if [ -a src/main/docker/mongodb.yml ]; then
     docker-compose -f src/main/docker/mongodb.yml up -d
+fi
+if [ -a src/main/docker/neo4j.yml ]; then
+    docker-compose -f src/main/docker/neo4j.yml up -d
 fi
 if [ -a src/main/docker/couchbase.yml ]; then
     # this container can't be started otherwise, it will be conflict with tests
@@ -28,17 +39,17 @@ fi
 if [ -a src/main/docker/postgresql.yml ]; then
     docker-compose -f src/main/docker/postgresql.yml up -d
 fi
-if [ -a src/main/docker/elasticsearch.yml ]; then
-    docker-compose -f src/main/docker/elasticsearch.yml up -d
-fi
 if [ -a src/main/docker/mariadb.yml ]; then
     docker-compose -f src/main/docker/mariadb.yml up -d
 fi
-if [ -a src/main/docker/kafka.yml ]; then
-    docker-compose -f src/main/docker/kafka.yml up -d
+if [ -a src/main/docker/redis.yml ]; then
+    docker-compose -f src/main/docker/redis.yml up -d
 fi
-if [ -a src/main/docker/keycloak.yml ]; then
-    docker-compose -f src/main/docker/keycloak.yml up -d
+if [ -a src/main/docker/memcached.yml ]; then
+    docker-compose -f src/main/docker/memcached.yml up -d
+fi
+if [ -a src/main/docker/jhipster-registry.yml ]; then
+    docker-compose -f src/main/docker/jhipster-registry.yml up -d
 fi
 
 docker ps -a
