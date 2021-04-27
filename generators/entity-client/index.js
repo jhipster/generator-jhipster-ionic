8620d395d8d4b6a297ff7e9a5dedcff797643048
Marcelo Shima <marceloshima@gmail.com>
2021-04-27 18:05:36 -0300

    Drop removed api.

--- generators/entity-client/index.js
+++ generators/entity-client/index.js
@@ -221,7 +221,7 @@ module.exports = class extends BaseGenerator {
         query = `this.${relationship.otherEntityName}Service.query()
             .subscribe(data => { this.${variableName} = data.body; }, (error) => this.onError(error));`;
       }
-      if (variableName && !this.contains(queries, query)) {
+      if (variableName && !queries.includes(query)) {
         queries.push(query);
         variables.push(`${variableName}: ${relationship.otherEntityAngularName}[];`);
       }
