4ef5ca520badc6a345d4fc8d6e3882a829f4cb16
Yadong Zhang <zhyd007@gmail.com>
2022-04-07 02:51:44 +0000

    test: jwt cypress.

--- generators/ionic/resources/jwt/e2e/spec/login.e2e-spec.ts
+++ generators/ionic/resources/jwt/e2e/spec/login.e2e-spec.ts
@@ -1,4 +1,3 @@
-import { browser, by, element, ExpectedConditions as ec } from 'protractor';
 import { LoginPage } from '../pages/login.po';
 
 describe('Login', () => {
@@ -6,41 +5,38 @@ describe('Login', () => {
   const username = process.env.E2E_USERNAME || 'admin';
   const password = process.env.E2E_PASSWORD || 'admin';
 
-  beforeEach(async () => {
+  beforeEach(() => {
     loginPage = new LoginPage();
-    await loginPage.navigateTo('/');
+    loginPage.navigateTo('/');
   });
 
-  it('should show a login button', async () => {
-    expect(loginPage.getHeader()).toMatch(/Welcome, Java Hipster/);
-    expect(await loginPage.loginButton.isPresent());
+  it('should show a login button', () => {
+    loginPage.getHeader().invoke('text').should('match', /Welcome, Java Hipster/);
+    loginPage.loginButton.should('exist');
   });
 
-  it('should fail to login with bad password', async () => {
-    await browser.wait(ec.visibilityOf(loginPage.signInButton));
-    await loginPage.signInButton.click();
-    await loginPage.login(username, 'foo');
-    const error = element(by.css('.toast-message'));
-    if (await error.isPresent()) {
-      expect(await error.getText()).toMatch(/Unable to sign in/);
+  it('should fail to login with bad password', () => {
+    loginPage.signInButton.click();
+    loginPage.login(username, 'foo');
+    const error = cy.get('.toast-message');
+    if (error) {
+      error.invoke('text').should('match', /Unable to sign in/);
     }
   });
 
-  it('should login successfully with admin account', async () => {
-    await browser.wait(ec.visibilityOf(loginPage.signInButton));
-    await loginPage.signInButton.click();
-    await loginPage.login(username, password);
+  it('should login successfully with admin account', () => {
+    loginPage.signInButton.click();
+    loginPage.login(username, password);
 
     const welcome = /Welcome, Administrator/;
-    await browser.wait(ec.visibilityOf(loginPage.logoutButton));
-    expect(element.all(by.css('ion-title')).getText()).toMatch(welcome);
+    cy.get('.ion-title').invoke('text').should('match', welcome);
   });
 
-  it('should logout successfully', async () => {
-    if (await loginPage.logoutButton.isPresent()) {
-      await loginPage.logout();
-      await browser.wait(ec.urlContains('/'));
-      expect(await loginPage.signInButton.isPresent());
+  it('should logout successfully', () => {
+    if (loginPage.logoutButton) {
+      loginPage.logout();
+      cy.url().should('include', '/');
+      loginPage.signInButton.shoule('exist');
     }
   });
 });
