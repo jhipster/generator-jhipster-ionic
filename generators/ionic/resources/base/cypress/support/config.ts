9b2cd452f9b4da2ff25fd31dc8e27f670872697d
Marcelo Shima <marceloshima@gmail.com>
2022-04-29 11:51:33 -0300

    Environment variables required Cypress.env().

--- generators/ionic/resources/base/cypress/support/config.ts
+++ generators/ionic/resources/base/cypress/support/config.ts
@@ -1,9 +1,9 @@
 import { apiHost as environmentHost } from '../../src/environments/environment';
 
-export const USER_USERNAME = process.env.E2E_USERNAME || 'user';
-export const USER_PASSWORD = process.env.E2E_PASSWORD || 'user';
+export const USER_USERNAME = Cypress.env('E2E_USERNAME') || 'user';
+export const USER_PASSWORD = Cypress.env('E2E_PASSWORD') || 'user';
 
-export const ADMIN_USERNAME = process.env.ADMIN_E2E_USERNAME || process.env.E2E_USERNAME || 'admin';
-export const ADMIN_PASSWORD = process.env.ADMIN_E2E_PASSWORD || process.env.E2E_PASSWORD || 'admin';
+export const ADMIN_USERNAME = Cypress.env('ADMIN_E2E_USERNAME') || Cypress.env('E2E_USERNAME') || 'admin';
+export const ADMIN_PASSWORD = Cypress.env('ADMIN_E2E_PASSWORD') || Cypress.env('E2E_PASSWORD') || 'admin';
 
-export const apiHost = process.env.API_HOST || environmentHost.replace(/\/$/, '');
+export const apiHost = Cypress.env('API_HOST') || environmentHost.replace(/\/$/, '');
