2facd14170e9eefdf19c81df4ca98e9f90cd1b96
Marcelo Shima <marceloshima@gmail.com>
2022-04-28 21:30:54 -0300

    Fix typo

--- generators/ionic/resources/base/cypress/support/pages/app.po.ts
+++ generators/ionic/resources/base/cypress/support/pages/app.po.ts
@@ -1,11 +1,11 @@
 export class Page {
-  pageSeletor: string;
+  pageSelector: string;
 
   back() {
-    cy.get(`${this.pageSeletor} ion-back-button`).click();
+    cy.get(`${this.pageSelector} ion-back-button`).click();
   }
 
   getPageTitle() {
-    return cy.get(`${this.pageSeletor} ion-title`).should('be.visible');
+    return cy.get(`${this.pageSelector} ion-title`).should('be.visible');
   }
 }
--- generators/ionic/resources/base/cypress/support/pages/entity.po.ts
+++ generators/ionic/resources/base/cypress/support/pages/entity.po.ts
@@ -2,40 +2,40 @@ import { Page } from './app.po';
 
 export class EntityComponentsPage extends Page {
   clickOnCreateButton() {
-    cy.get(`${this.pageSeletor } ion-fab-button`).click();
+    cy.get(`${this.pageSelector } ion-fab-button`).click();
   }
 }
 
 export class EntityUpdatePage extends Page {
   save() {
-    cy.get(`${this.pageSeletor} ion-buttons[slot="end"] ion-button`).click();
+    cy.get(`${this.pageSelector} ion-buttons[slot="end"] ion-button`).click();
   }
 
   setInputValue(formControlName: string, value: string) {
-    cy.get(`${this.pageSeletor} ion-input[formControlName="${formControlName}"] input`).type(value);
+    cy.get(`${this.pageSelector} ion-input[formControlName="${formControlName}"] input`).type(value);
   }
 
   setDateTime(formControlName: string, value: string) {
-    // cy.get(`${this.pageSeletor} ion-datetime[formControlName="${formControlName}"] input`).type(value);
+    // cy.get(`${this.pageSelector} ion-datetime[formControlName="${formControlName}"] input`).type(value);
   }
 
   setDate(formControlName: string, value: string) {
-    // cy.get(`${this.pageSeletor} ion-datetime[formControlName="${formControlName}"] input`).type(value);
+    // cy.get(`${this.pageSelector} ion-datetime[formControlName="${formControlName}"] input`).type(value);
   }
 
   setTextAreaContent(formControlName: string, value: string) {
-    cy.get(`${this.pageSeletor} ion-textarea[formControlName="${formControlName}"] textarea`).type(value);
+    cy.get(`${this.pageSelector} ion-textarea[formControlName="${formControlName}"] textarea`).type(value);
   }
 
   setBoolean(formControlName: string, value: string) {
-    cy.get(`${this.pageSeletor} ion-checkbox[formControlName="${formControlName}"] checkbox`).click();
+    cy.get(`${this.pageSelector} ion-checkbox[formControlName="${formControlName}"] checkbox`).click();
   }
 
   setBlob(inputName: string, fileName: string) {
     const mimeType = 'image/png';
     cy.fixture('integration-test.png')
       .as('image')
-      .get(`${this.pageSeletor} input[data-cy="${inputName}"]`)
+      .get(`${this.pageSelector} input[data-cy="${inputName}"]`)
       .then(function (el) {
         const blob = Cypress.Blob.base64StringToBlob(this.image, mimeType);
         const file = new File([blob], fileName, { type: mimeType });
@@ -48,7 +48,7 @@ export class EntityUpdatePage extends Page {
   }
 
   select(formControlName: string, value: string) {
-    cy.get(`${this.pageSeletor} ion-select[formControlName="${formControlName}"]`).click();
+    cy.get(`${this.pageSelector} ion-select[formControlName="${formControlName}"]`).click();
     cy.get('ion-alert div.alert-radio-group button').contains(value).first().click();
     cy.get('ion-alert div.alert-button-group button:not(.alert-button-role-cancel)').click();
   }
@@ -56,10 +56,10 @@ export class EntityUpdatePage extends Page {
 
 export class EntityDetailPage extends Page {
   edit() {
-    cy.get(`${this.pageSeletor } ion-button[color="primary"]`).click();
+    cy.get(`${this.pageSelector } ion-button[color="primary"]`).click();
   }
 
   delete() {
-    cy.get(`${this.pageSeletor } ion-button[color="danger"]`).click();
+    cy.get(`${this.pageSelector } ion-button[color="danger"]`).click();
   }
 }
