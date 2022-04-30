19427cbbafb533f643d842e76bbb906f3f2977c1
Matt Raible <matt.raible@okta.com>
2022-04-30 14:25:39 -0600

    Use API for Keycloak login

--- generators/ionic/resources/oauth2/cypress/support/commands.ts
+++ generators/ionic/resources/oauth2/cypress/support/commands.ts
@@ -22,34 +22,66 @@ const authenticatedRequest = (data: any) => {
 
 const login = (username: string, password: string) => {
   cy.getOauth2Data().then(oauth2Data => {
-    const { configuration: { authorization_endpoint } } = oauth2Data;
-    cy.session(
-      [username, password],
-      () => {
-        cy.visit('/');
-        cy.get('#signIn').click();
-        cy.origin(authorization_endpoint, {
-          args: { authorization_endpoint, username, password }
-          // eslint-disable-next-line @typescript-eslint/no-shadow
-        }, ({ authorization_endpoint, username, password }) => {
-          let usernameElement = 'username';
-          let passwordElement = 'password';
-          if (authorization_endpoint.includes('okta')) {
-            usernameElement = 'identifier';
-            passwordElement = 'credentials.passcode';
-          }
-          cy.get(`input[name="${usernameElement}"]`).type(username);
-          cy.get(`input[name="${passwordElement}"]`).type(password);
-          cy.get('[type="submit"]').first().click();
+    const {
+      info: { clientId },
+      configuration: { authorization_endpoint, token_endpoint },
+    } = oauth2Data;
+    // Keycloak is special because https://github.com/cypress-io/cypress/issues/21201
+    if (authorization_endpoint.includes('realm')) {
+      const authorizeCode = cy.keycloakLogin(oauth2Data, username, password);
+      authorizeCode.then(({ headers }) => {
+        const { location } = headers;
+        const locationUrl = new URL(location as string);
+        const code = locationUrl.searchParams.get('code');
+
+        // Retrieve token.
+        cy.request({
+          method: 'POST',
+          url: token_endpoint,
+          followRedirect: false,
+          form: true,
+          body: {
+            grant_type: 'authorization_code',
+            code,
+            refresh_token: undefined,
+            redirect_uri: window.location.origin,
+            client_id: clientId,
+          },
+        }).then(({ body }) => {
+          localStorage.setItem('CapacitorStorage.token_response', JSON.stringify(body));
+          cy.visit('/');
+          cy.url().should('eq', Cypress.config().baseUrl + 'tabs/home');
         });
-        cy.url({timeout: 10000}).should('eq', Cypress.config().baseUrl + 'tabs/home');
-      },
-      {
-        validate: () => {
-          cy.authenticatedRequest({url: '/api/account'}).its('status').should('eq', 200);
+      });
+    } else {
+      cy.session(
+        [username, password],
+        () => {
+          cy.visit('/');
+          cy.get('#signIn').click();
+          cy.origin(authorization_endpoint, {
+            args: {authorization_endpoint, username, password}
+            // eslint-disable-next-line @typescript-eslint/no-shadow
+          }, ({authorization_endpoint, username, password}) => {
+            let usernameElement = 'username';
+            let passwordElement = 'password';
+            if (authorization_endpoint.includes('okta')) {
+              usernameElement = 'identifier';
+              passwordElement = 'credentials.passcode';
+            }
+            cy.get(`input[name="${usernameElement}"]`).type(username);
+            cy.get(`input[name="${passwordElement}"]`).type(password);
+            cy.get('[type="submit"]').first().click();
+          });
+          cy.url({timeout: 10000}).should('eq', Cypress.config().baseUrl + 'tabs/home');
         },
-      }
-    );
+        {
+          validate: () => {
+            cy.authenticatedRequest({url: '/api/account'}).its('status').should('eq', 200);
+          },
+        }
+      );
+    }
   });
 };
 
--- generators/ionic/resources/oauth2/cypress/support/oauth2.ts
+++ generators/ionic/resources/oauth2/cypress/support/oauth2.ts
@@ -2,6 +2,11 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="cypress" />
 import { apiHost } from './config';
+import { environment } from '../../src/environments/environment';
+
+const {
+  oidcConfig: { redirect_url: redirect_uri, scopes: scope, audience, client_id },
+} = environment;
 
 // Get oauth2 basic data
 const getOauth2Data = () =>
@@ -18,12 +23,49 @@ const getOauth2Data = () =>
           followRedirect: false,
         })
         .then(({ body: configuration }) => ({
-          configuration
+          info,
+          configuration,
+          qs: {
+            redirect_uri: window.location.origin,
+            client_id,
+            response_type: 'code',
+            scope,
+            audience,
+          },
         }));
     });
 
+// Get authorization from keycloak
+const keycloakLogin = ({ configuration, qs }, username: string, password: string) => {
+  const { authorization_endpoint } = configuration;
+  return cy
+    .request({
+      url: authorization_endpoint,
+      qs,
+      followRedirect: false,
+    })
+    .then(response => {
+      const html = document.createElement('html');
+      html.innerHTML = response.body;
+
+      const form = html.getElementsByTagName('form')[0];
+      const url = form.action;
+      return cy.request({
+        method: 'POST',
+        url,
+        followRedirect: false,
+        form: true,
+        body: {
+          username,
+          password,
+        },
+      });
+    });
+};
+
 Cypress.Commands.addAll({
   getOauth2Data,
+  keycloakLogin
 });
 
 declare global {
