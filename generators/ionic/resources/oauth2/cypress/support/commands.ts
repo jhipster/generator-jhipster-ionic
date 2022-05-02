0cdb966864d1790ad462f2bdad1526a7d0ab51b9
Matt Raible <matt.raible@okta.com>
2022-05-02 11:15:41 -0600

    Allow Okta Classic and OIE (#665)

--- generators/ionic/resources/oauth2/cypress/support/commands.ts
+++ generators/ionic/resources/oauth2/cypress/support/commands.ts
@@ -65,8 +65,15 @@ const login = (username: string, password: string) => {
             let usernameElement = 'username';
             let passwordElement = 'password';
             if (authorization_endpoint.includes('okta')) {
-              usernameElement = 'identifier';
-              passwordElement = 'credentials.passcode';
+              // Element names have changed for Okta Identity Engine.
+              // Identity-first flow might require additional changes, for example:
+              // https://github.com/okta/okta-react/blob/master/test/e2e/page-objects/okta-oie-signin-page.js#L30
+              cy.get('body').then(body => {
+                if (body.find('input[name="identifier"]').length) {
+                  usernameElement = 'identifier';
+                  passwordElement = 'credentials.passcode';
+                }
+              });
             }
             cy.get(`input[name="${usernameElement}"]`).type(username);
             cy.get(`input[name="${passwordElement}"]`).type(password);
