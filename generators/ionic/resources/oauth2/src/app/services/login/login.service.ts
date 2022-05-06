4a771dceeb5afc17d04ca9aa554d7ec0560c7669
Matt Raible <matt.raible@okta.com>
2022-05-06 14:51:42 -0600

    Fix logout on Auth0

--- generators/ionic/resources/oauth2/src/app/services/login/login.service.ts
+++ generators/ionic/resources/oauth2/src/app/services/login/login.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AccountService } from '../auth/account.service';
-import { AuthService } from 'ionic-appauth';
+import { AuthService, Browser } from 'ionic-appauth';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor(private accountService: AccountService, private authService: AuthService, private translate: TranslateService) {}
+  constructor(private accountService: AccountService, private authService: AuthService,
+              private translate: TranslateService, private browser: Browser) {}
 
   login() {
     this.authService
@@ -29,5 +31,14 @@ export class LoginService {
   async logout() {
     await this.authService.signOut();
     this.accountService.authenticate(null);
+
+    // Auth0 need special handling since end_session_endpoint is not in oidc-configuration
+    const issuer = environment.oidcConfig.server_host;
+    if (issuer.includes('auth0.com')) {
+      const clientId = environment.oidcConfig.client_id;
+      const returnTo = encodeURIComponent(environment.oidcConfig.end_session_redirect_url);
+      const logoutUrl = `${issuer}/v2/logout?client_id=${clientId}&returnTo=${returnTo}`;
+      await this.browser.showWindow(logoutUrl, returnTo);
+    }
   }
 }
