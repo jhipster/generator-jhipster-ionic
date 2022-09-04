14cdd879fb6ef0c04ccb6ec01caeafb9e7703d55
Marcelo Shima <marceloshima@gmail.com>
2022-09-04 19:43:46 -0300

    migrate camera to capacitor.

--- generators/ionic/resources/base/src/main.ts
+++ generators/ionic/resources/base/src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -11,3 +12,5 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
+
+defineCustomElements(window);
