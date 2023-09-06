592ead752affe937556dbf699bf3b2c3d28dea77
Marcelo Shima <marceloshima@gmail.com>
2023-09-06 15:19:56 -0300

    add dev-blueprint

--- cli/cli.mjs
+++ cli/cli.mjs
@@ -3,12 +3,13 @@
 import { runJHipster, done, logger } from 'generator-jhipster/esm/cli';
 import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
-import { dirname, basename } from 'path';
+import { dirname, basename, join } from 'path';
 
 // Get package name to use as namespace.
 // Allows blueprints to be aliased.
 const packagePath = dirname(dirname(fileURLToPath(import.meta.url)));
 const packageFolderName = basename(packagePath);
+const devBlueprintPath = join(packagePath, '.blueprint');
 
 (async () => {
   const { version, bin } = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
@@ -18,6 +19,7 @@ const packageFolderName = basename(packagePath);
     executableName,
     executableVersion: version,
     defaultCommand: 'ionic',
+    devBlueprintPath,
     blueprints: {
       [packageFolderName]: version,
     },
